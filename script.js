f75c025bb0ed888d193fd6006662d0bdab7825e8
reese_s <icipherfly@gmail.com>
2020-08-26 21:53:15 -0700

    COmpleted the .fadeIn and .hide portions of the project.  Will review.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // JQUERY FUNCTION //
 
 $(document).ready(function()
-{ 
-    // ESTABLISH OBJECT 
+{    
+        // ESTABLISH OBJECT 
     var oracle = {};
-    // CREATE A PROPERTY OF THE OBJECT AND IT'S ARRAY OF ANSWERS 
+        // CREATE A PROPERTY OF THE OBJECT AND IT'S ARRAY OF ANSWERS 
     oracle.answers = 
     [
         "Concentrate harder the answer will come to you.", 
@@ -18,47 +18,36 @@ $(document).ready(function()
         "Well, what do YOU think?", 
         "It tickles my JavaScript when you ask me that, do it again!"
     ];
-
-
-    // CREATE A METHOD TO PRODUCE RANDOM ANSWERS FROM ARRAY//
-    oracle.prophesize = function(question){
-                                            
-    // FORMULA //
-    // CREATE RANDOM NUMBER BETWEEN 0 AND 1 //
-    var randomNumber = Math.random();
-    // CREATE RANDOM NUMBER BETWEEN 0 AND LENNGTH OF ARRAY//
-    var randomNumberArray = randomNumber * this.answers.length;
-    // ROUND THAT NUMBER DOWN TO A WHOLE NUMBER //
-    var randomIndex = Math.floor (randomNumberArray);
-    // CREATE VARIABLE FOR THE RANDOM NUMBER //
-    var randomAnswer = this.answers[randomIndex];
-
+    $("#answer").hide();
+     
+        // CREATE A METHOD TO PRODUCE RANDOM ANSWERS FROM ARRAY//
+    oracle.prophesize = function(question){ 
+    $("#answer").fadeIn(4000);                                 
+                // FORMULA //
+        // CREATE RANDOM NUMBER BETWEEN 0 AND 1 //
+        var randomNumber = Math.random();
+        // CREATE RANDOM NUMBER BETWEEN 0 AND LENNGTH OF ARRAY//
+        var randomNumberArray = randomNumber * this.answers.length;
+        // ROUND THAT NUMBER DOWN TO A WHOLE NUMBER //
+        var randomIndex = Math.floor (randomNumberArray);
+        // CREATE VARIABLE FOR THE RANDOM NUMBER //
+        var randomAnswer = this.answers[randomIndex];
     
-
-    // PROMPT QUESTION AND ANSWER FROM INSIDE FUNCTION FORMULA
+        //  JQ FORMULA TO GET THE ANSWER TO APPEAR //
+    $("#answer").text(randomAnswer);
+    
+        // PROMPT QUESTION AND ANSWER FROM INSIDE FUNCTION FORMULA
     console.log (question);
     console.log (randomAnswer);
-
-    $("#answer").text(randomAnswer);
     };   
-
-    // PROMPT JQ FORMULA | QUESTION BUTTON//
-
+    
+        // PROMPT JQ FORMULA | QUESTION BUTTON//
     var askMe = function(){
+    $("#answer").hide();
         var question = prompt("Let's have some fun, ask me a YES/NO question!");
         oracle.prophesize(question);
     };
-        $("#questionButton").click(askMe);
-
-    //  JQ FORMULA TO GET THE ANSWER TO APPEAR //
-
-    
-    /*  JQ FORMULA TO GET THE ANSWER TO APPEAR 
-  var randomAnswer = function() {
-    oracle.answers(randomAnswer);
-     oracle.answers(answer);
-     $("#answer").text(randomAnswer);
-  };*/
-  
-
+    $("#answer").hide();
+    $("#questionButton").click(askMe);
+     
 });
